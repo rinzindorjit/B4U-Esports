ed32b82e448b338c0d93029fd3fed640a48a1eb7
Rinzin Dorji <iyus.rigzin901@gmail.com>
2025-05-31 14:27:24 +0100

    Update config.js

--- pages/api/config.js
+++ pages/api/config.js
@@ -3,6 +3,5 @@ export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'OPTIONS') return res.status(200).end();
-    
     res.status(200).json({ appId: 'b4uesports' });
 }
